23f959630dbd7316df4f2a81fc30664b679e6611
dmitrii linkevich <bratok@DESKTOP-2KQHT7U.localdomain>
2023-03-29 20:19:28 +0300

    step 4. fix with eslint yml, brain-games, cli

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
+// импорт функции
+import sayYourName from '../src/cli.js';
+
 console.log('Welcome to the Brain Games!');
-// импорт функции 
-import { sayYourName } from '../src/cli.js';
 // вызов функции
 sayYourName();
-
--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
-//импорт библиотеки readline-sync из ранее установленной через npm istall 
+// импорт библиотеки readline-sync из ранее установленной через npm istall
 import readlineSync from 'readline-sync';
 
 const sayYourName = () => {
-const userName = readlineSync.question('May I have your name?: ');
-console.log('Hi, ' + userName + '!');
+  const userName = readlineSync.question('May I have your name?: ');
+  console.log(`Hi, ${userName}!`);
 };
-export { sayYourName };
+export default sayYourName;
