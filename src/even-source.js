d1870d34df66384fcc26523df87a9f60bc70d97b
dmitrii linkevich <bratok@DESKTOP-2KQHT7U.localdomain>
2023-03-31 00:25:34 +0300

    step 5. auto fix lint

--- src/even-source.js
+++ src/even-source.js
@@ -1,29 +1,27 @@
 // импорт библиотеки readline-sync из ранее установленной через npm istall
 import readlineSync from 'readline-sync';
 
-//генератор случайных чисел из диапазона от min до max
-const getRandomNumber = (min, max) => {     
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+// генератор случайных чисел из диапазона от min до max
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 const brainEven = () => {
-    const userName = readlineSync.question('May I have your name?: ');
-    console.log(`Hi, ${userName}!`);
+  const userName = readlineSync.question('May I have your name?: ');
+  console.log(`Hi, ${userName}!`);
 
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
-   
-    for (let i = 0; i < 3; i += 1) {
-        let rand = getRandomNumber(-100, 100);
-        console.log(`Question: ${rand}`);
-        let answer = readlineSync.question('Your answer: ');
-        if (answer == 'yes' && rand % 2 === 0 || answer == 'no' && rand % 2 !== 0) {
-            console.log('Correct!');
-        } else {console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
-        console.log(`Let's try again, ${userName}!`);
-        return;
-        }
-    } console.log(`Congratulations, ${userName}!`);
-    return;
-}; 
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  for (let i = 0; i < 3; i += 1) {
+    const rand = getRandomNumber(-100, 100);
+    console.log(`Question: ${rand}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (answer == 'yes' && rand % 2 === 0 || answer == 'no' && rand % 2 !== 0) {
+      console.log('Correct!');
+    } else {
+      console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+  } console.log(`Congratulations, ${userName}!`);
+};
 
 export default brainEven;
