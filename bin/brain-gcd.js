e056e3450ce3d49a2b01da82b011d2a1df9e435d
Dmitrii Linkevich <bratokzs@yandex.ru>
2023-04-17 22:24:51 +0300

    step 7. Edit brain-gcd, gcd-source, package.json. Euclids algorithm habr

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,3 +1,3 @@
 #!/usr/bin/env node
 // импорт модуля игры для реализации побочных эффектов
-import '../src/games/calc-source.js';
+import '../src/games/gcd-source.js';
--- src/games/gcd-source.js
+++ src/games/gcd-source.js
@@ -4,28 +4,29 @@ import game from '../index.js';
 import getRandomNumber from '../getRandomNumber.js';
 
 function gameData() {
-  const arg1 = getRandomNumber(-100, 100);
-  const arg2 = getRandomNumber(-100, 100);
+  const arg1 = getRandomNumber(-20, 20);
+  const arg2 = getRandomNumber(-20, 20);
   const question = `${arg1} ${arg2}`;
-  
+
   const result = () => {
-    };
+    // классический алгоритм Евклида
+    if (arg1 === arg2) {
+      return arg1;
+    }
+    let number1 = Math.abs(arg1);
+    let number2 = Math.abs(arg2);
+    while (number1 !== 0 && number2 !== 0) {
+      if (number1 > number2) {
+        number1 -= number2;
+      } else {
+        number2 -= number1;
+      }
+    }
+    return String(number1 + number2);
+  };
   return [question, result()];
-  }
-  
-  const rules = 'Find the greatest common divisor of given numbers.';
-  
-  export default game(gameData, rules);
+}
 
+const rules = 'Find the greatest common divisor of given numbers.';
 
-
-//  const result = () => {
-//    if (`${oper}` === '+') {
-//      return String(arg1 + arg2);
-//    } if (`${oper}` === '-') {
-//      return String(arg1 - arg2);
-//    }
-//    return String(arg1 * arg2);
-//  };
-//  return [question, result()];
-// }
+export default game(gameData, rules);
