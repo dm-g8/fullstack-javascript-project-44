23416b25ef6a82badd121b738d0ad243d7997c35
Dmitrii Linkevich <bratokzs@yandex.ru>
2023-04-20 00:09:02 +0300

    step 8. add mass[] to progression-source

--- src/games/progression-source.js
+++ src/games/progression-source.js
@@ -9,29 +9,24 @@ function gameData() {
   const delta = getRandomNumber(-10, 10);
   const length = getRandomNumber(5, 10);
   const randomPlace = getRandomNumber(1, length);
+  console.log(randomPlace);
+  const startArg = arg1;
 
-
-
-  let question = `${arg1} `;
-
+  let mass = [];
   for (let i = 1; i <= length; i += 1) {
-    if (i = randomPlace) {
-      arg1 = arg1 + delta;
-      question = question + `.. `;
-    } else {
-      arg1 = arg1 + delta;
-      question = question + `${arg1} `;
-    };
+    mass.push(arg1);
+    arg1 += delta;
+  }
+  mass[randomPlace] = '..';
+
+  let question = ' ';
+  for (let j = 1; j < length; j += 1) {
+    question += `${mass[j]} `;
   };
 
   const result = () => {
-//    if (`${oper}` === '+') {
-//      return String(arg1 + arg2);
-//    } if (`${oper}` === '-') {
-//      return String(arg1 - arg2);
-//    }
-//    return String(arg1 * arg2);
-  };
+  return String(startArg + delta * randomPlace);
+};
   return [question, result()];
 }
 
